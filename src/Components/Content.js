c6b3a9f378c442ba85a7dfb26e902ef97579a10a
Gustavo Sánchez <gustavo.sanchez@korinver.com>
2018-10-09 11:27:58 -0600

    Consuming API

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
 import '../Global/css/Content.css';
 
+const Results = ( props ) =>(
+  <li>{props.name}</li>
+)
+
 class Content extends Component {
   constructor(){
     super();
+    this.state = {
+      results: []
+    }
   }
 
-componentWillMount(){
-  
+componentDidMount(){
+  fetch('https://pokeapi.co/api/v2/pokemon/')
+    .then(response => response.json())
+    .then(results => {
+      results.results.forEach(results => {
+        let data = {
+          name:results.name,
+          url:results.url
+        }
+        this.setState({ results:this.state.results.concat([data])})
+      })
+    })
 }
 
   render() {
+
+    if (this.state.results.length > 0) {
+      return (
+
+        <div className="Content">
+          <h2>Here comes a entire world of Pokémon</h2>
+          <div className="pokeContent">
+
+            <div className="subPokeContent1">
+              <div>
+                { this.state.results.map(results => <Results name={results.name} url={results.url}/>) }
+              </div>
+            </div>
+            <div className="subPokeContent2">
+              <p>Hola</p>
+            </div>
+            <div className="subPokeContent3">
+              <p>Hola</p>
+            </div>
+          </div>
+        </div>
+
+      );
+    }
     return (
       <div className="Content">
         <h2>Here comes a entire world of Pokémon</h2>
         <div className="pokeContent">
+
           <div className="subPokeContent1">
-            <p>Hola</p>
+
           </div>
           <div className="subPokeContent2">
             <p>Hola</p>
